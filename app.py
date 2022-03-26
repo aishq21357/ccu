9c4dba779d1e954ea46538e9a5145480d4a4cc24
whatevermuffin <39726747+whatevermuffin@users.noreply.github.com>
2022-03-26 16:32:26 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[38]:
+
+
+from flask import Flask, request, render_template
+import joblib
+
+
+# In[39]:
+
+
+app = Flask(__name__)
+
+
+# In[40]:
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        purchases = request.form.get("purchases")
+        suppcard = request.form.get("suppcard")
+        print(purchases, suppcard)
+        purchases = float(purchases)
+        suppcard = float(suppcard)
+        
+        model1 = joblib.load("cart")
+        pred1 = model1.predict([[purchases, suppcard]])
+        model2 = joblib.load("RandomForest")
+        pred2 = model2.predict([[purchases, suppcard]])
+        model3 = joblib.load("GradientBoostingClassifier")
+        pred3 = model3.predict([[purchases, suppcard]])
+        
+        
+        return(render_template("index.html", result1 = pred1, result2 = pred2, result3 = pred3))
+    else:
+        return(render_template("index.html", result1 = "2", result2 = "2", result3 = "2"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
